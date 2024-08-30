f27288469d406b5ff3c57db4f452df0b5d5322c4
abhi10 <154737521+abcd799@users.noreply.github.com>
2024-08-30 20:06:39 +0530

    modify default news image

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import DefaultImage from '../newsimage.jpg'
 
 function NewsItem(props) {
   const { id, image_url, title, summary, url } = props;
-
+  
+  const handleImageError=(event)=>{
+      event.target.src=DefaultImage
+  }
+  
   return (
     <>
       <div key={id} className='news-item'>
-        <img src={image_url} alt=''/>
+        <img src={image_url} alt='newshere' onError={handleImageError}/>
         <div className='title'>{title}</div>
         <div className='summary'> {summary.length > 100 ? summary.substring(0, 100) + "....." : summary} </div>
         <button className='read-btn' onClick={() => window.location.href = url}>Read More
